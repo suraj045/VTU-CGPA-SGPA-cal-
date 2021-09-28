06ebb888edcafd825cf48b2fbd9889dd3b34c74f
Shreyas <shreyasbabshet89@gmail.com>
2021-09-28 20:03:34 +0530

    First commit

---
+++ mainapp/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+     path('',views.login, name = 'index'),
+     path('register',views.register,name='register'),
+     path('login',views.login,name='login'),
+     path('logout',views.logout,name='logout'),
+     path('sgpa',views.sgpa),
+     path('home',views.home),
+     path('cgpa',views.cgpa),
+     path('calc',views.calc,name='calc'),
+     path('contact',views.contact),
+]
